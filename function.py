1d6e830650709c8f3da25bc89115b28572d352bd
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2024-03-18 08:53:21 +0100

    up to date 18

--- function.py
+++ function.py
@@ -8,3 +8,42 @@ def greeting(name,greet='Aloha'):
 
 greeting('Dhanuka')
 
+
+#named argument
+
+def printInfo(name,age):
+    print(f'{name} {age}')
+
+printInfo(name='Dhanuka',age=24)
+
+#return value
+def sum(a,b):
+    '''
+    Takes 2 integers, return their sum
+    >>> sum(1,2)
+
+    '''
+    return a+b
+
+
+print('Sum = '+ str(sum(1,2)))
+
+
+def tipCalculate():
+    foodAmount = float(input("Enter Food amount : "))
+    return foodAmount+ foodAmount* 0.10
+
+
+print("Food Tip Amount is : "+str((tipCalculate())))
+
+def weatherReport(value):
+
+    if(value=='cloudy'):
+        return 'cool'
+    elif(value=='sunny'):
+        return 'hot'
+    else:
+        return 'nothing'
+
+print(weatherReport('cloudy'))
+    
