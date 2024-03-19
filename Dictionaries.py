2ac59d35438abba3cbd71c0ba234e61363fb04ca
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2024-03-19 11:30:47 +0100

    up to date 03/19 touple/Set

--- Dictionaries.py
+++ Dictionaries.py
@@ -8,12 +8,25 @@
 #print(person['shirt'])
 #print(person['laptop'])
 
+
+def netWorth():
+    return person['asset'] - person['debt']
+
+
 def introducer():
     person = {
         'name':'Dhanuka',
         'shirt':'black',
-        'laptop':'apple'
+        'laptop':'apple',
+        'asset' : 100,
+        'debt': 50,
+        'networth' : lambda : person['asset'] - person['debt'],
+        'fruits': ['apple', 'mango', 'orange', 'banana']
+        
     }
-    print(f"Hi I am {person['name']}, I am wearing a {person['shirt']} color shirt, my laptop is {person['laptop']}  model")
+
+    
+
+    print(f"Hi I am {person['name']}, I am wearing a {person['shirt']} color shirt, my laptop is {person['laptop']}  model and my networth is {person['networth']()} and I like {person['fruits']}")
 
 introducer()
---
+++ Touple.py
@@ -0,0 +1,28 @@
+#touples
+#immiutable - can not change data
+number = (1,2,3)
+
+print(number)
+print(number[0])
+
+
+#SETS  used for getting unique elements 
+
+
+list1 = ['ruby','python', 'java script']
+list2 = ['ruby', 'sql', 'java', 'javascript']
+
+###Special ketywords ###
+#list, len, max, min. dict, set
+
+programmingLanguages = list1+list2
+print(programmingLanguages)
+
+print({1,2,2,2,2,1,1,3,3})
+things = (1,2,3,1,1,1)
+print(1 in things)
+print(5 in things)
+
+print(set(programmingLanguages))
+
+
